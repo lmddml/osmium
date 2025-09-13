4eb26e595ff6600e7491c859845e988843afa6b9
lmddml <129541025+lmddml@users.noreply.github.com>
2025-09-13 22:09:17 +0200

    Restructure project

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import { two } from "./two.ts";
+
+const main = (name: string) => {
+	console.log(name);
+};
+
+main("Hello World");
+
+console.log(two());
---
+++ server/src/two.ts
@@ -0,0 +1,3 @@
+export const two = () => {
+	return "Hello World";
+};
