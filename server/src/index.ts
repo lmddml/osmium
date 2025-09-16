62fdecc0f0deaa779e3fc150200b60dea5514f14
lmddml <129541025+lmddml@users.noreply.github.com>
2025-09-16 19:55:40 +0200

    Remove old cold

--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { two } from "./two.ts";
-
-const main = (name: string) => {
-	console.log(name);
-};
-
-main("Hello World");
-
-console.log(two());
--- server/src/two.ts
+++
@@ -1,3 +0,0 @@
-export const two = () => {
-	return "Hello World";
-};
